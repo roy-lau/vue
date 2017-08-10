815b1f744d3dee2d7f5c65030cb4e32b00038fff
roy-lau <roy-lau@foxmail.com>
2017-08-10 18:00:23 +0800

    :clock1::zap:定时任务,每天自动上传项目:zap:---2017/08/10-18:00:02

--- project/shopping/src/components/layout.vue
+++ project/shopping/src/components/layout.vue
@@ -19,7 +19,7 @@
             <li class="nav-pile">|</li>
             <li @click="aboutClick">关于</li>
           </ul>
-        </div>  
+        </div>
       </div>
       </div>
   <!-- ##############################  头部布局 ################## 结束 -->
@@ -40,15 +40,15 @@
              <!-- 登陆弹框 -->
              <my-dialog :is-show="isShowLoginDialog" @on-close="closeDialog('isShowLoginDialog')">
                   <log-form @has-log="onSuccessLog" ></log-form>
-             </my-dialog> 
+             </my-dialog>
               <!-- 注册弹框 -->
              <my-dialog :is-show="isShowRegDialog" @on-close="closeDialog('isShowRegDialog')">
                   <reg-form ></reg-form>
-             </my-dialog> 
+             </my-dialog>
               <!-- 关于弹框 -->
              <my-dialog :is-show="isShowAboutDialog" @on-close="closeDialog('isShowAboutDialog')">
                 <p>本报告在调研数据的基础上，采用定性与定量相结合的方式深入分析了专车市场发展的驱动因素与阻碍因素、专车市场背后的产业格局、专车企业的竞争格局、用户对专车市场的依赖程度、专车对其他交通工具运力的补充效应等，通过这五个章节的研究反映专车市场的发展态势和面临的问题。报告力求客观、深入、准确地反映中国专车市场发展情况，为政府、企事业单位和社会各界提供决策依据。 </p>
-             </my-dialog> 
+             </my-dialog>
       <!-- ##############################  弹框组件 ################## 结束 -->
       </div>
   <!-- ##############################  底部布局 ################## 结束 -->
@@ -81,20 +81,20 @@
         this.isShowLoginDialog = true
       },
       regClick (){
-this.isShowRegDialog = true
+        this.isShowRegDialog = true
       },
       outClick (){
-this.isShowDialog = true
+        this.isShowDialog = true
       },
       aboutClick (){
-this.isShowAboutDialog = true
+        this.isShowAboutDialog = true
       },
       closeDialog (attr) {
         this[attr] = false
       },
       onSuccessLog (data) {
       console.log("输入的用户名为："+data)
-      this.closeDialog ('isShowLoginDialog')
+      this.closeDialog('isShowLoginDialog')
       this.username = data
     }
     }
@@ -112,8 +112,8 @@ this.isShowAboutDialog = true
     dl, dt, dd, ol, ul, li,
     fieldset, form, label, legend,
     table, caption, tbody, tfoot, thead, tr, th, td,
-    article, aside, canvas, details, embed, 
-    figure, figcaption, footer, header, hgroup, 
+    article, aside, canvas, details, embed,
+    figure, figcaption, footer, header, hgroup,
     menu, nav, output, ruby, section, summary,
     time, mark, audio, video {
       margin: 0;
@@ -124,7 +124,7 @@ this.isShowAboutDialog = true
       vertical-align: baseline;
     }
     /* HTML5 display-role reset for older browsers */
-    article, aside, details, figcaption, figure, 
+    article, aside, details, figcaption, figure,
     footer, header, hgroup, menu, nav, section {
       display: block;
     }
