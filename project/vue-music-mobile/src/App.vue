2161d92f59634b70292fcf810f8ba212b0d1d39a
roy-lau <roy-lau@foxmail.com>
2017-10-07 23:46:36 +0800

    music-player-vue：目录结构搭建，基础样式搭建

--- project/vue-music-mobile/src/App.vue
+++ project/vue-music-mobile/src/App.vue
@@ -5,19 +5,14 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: 'app'
-}
+<script type="text/ecmascript-6">
+
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import "~common/stylus/varible"
+
+#app
+  color: $color-theme
+
 </style>
