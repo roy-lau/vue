c352e1e8d7e700c99439c1a6aa6b9b6697001e7e
roy-lau <roy-lau@foxmail.com>
2017-10-08 11:04:30 +0800

    music-player-vue：m-header组价开发

--- project/vue-music-mobile/src/App.vue
+++ project/vue-music-mobile/src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    hello woeld
+    <m-header></m-header>
   </div>
 </template>
-
 <script type="text/ecmascript-6">
+import MHeader from '@/components/m-header/m-header'
+
+export default {
+  components: {
+    MHeader
+  }
+}
 
 </script>
-
 <style scoped lang="stylus" rel="stylesheet/stylus">
-@import "~common/stylus/variable"
-
-#app
-    color: $color-theme
 
 </style>
--- project/vue-music-mobile/src/main.js
+++ project/vue-music-mobile/src/main.js
@@ -1,10 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 // import router from './router'
-
+import fastclick from 'fastclick'
 import 'common/stylus/index.styl'
+
+fastclick.attach(document.body) // 解决移动端点击300毫秒延迟
 
 Vue.config.productionTip = false
 
