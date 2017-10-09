142f4647d6fc84f720f865c85975837cbd37d264
roy-lau <roy-lau@foxmail.com>
2017-10-09 12:11:49 +0800

    :clock1::zap:定时任务,每天自动上传项目:zap:---2017/10/09-12:11:19

--- project/vue-music-mobile/src/components/recommend/recommend.vue
+++ project/vue-music-mobile/src/components/recommend/recommend.vue
@@ -1,13 +1,54 @@
 <template>
   <div class="recommend">
-    <div>页面推荐</div>
+    <div class="recommend-content">
+        <div v-if="recommends.length" class="slider-wrapper">
+            <slider>
+              <div v-for="item in recommends">
+                <a :href="item.linkUrl"></a>
+                  <img :src="item.picUrl" alt="" />
+              </div>
+            </slider>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+
+          </ul>
+        </div>
+    </div>
   </div>
 </template>
-
 <script type="text/ecmascript-6">
+import slider from 'base/slider/slider'
+import { getRecommend } from 'api/recommend'
+import { ERR_OK } from 'api/config'
 
-  export default {}
+export default {
+  data() {
+    return {
+      recommends: []
+    }
+  },
+  created() {
+    this._getRecommend()
+  },
+  methods: {
+    _getRecommend() {
+      getRecommend().then((res) => {
+        if (res.code === ERR_OK) {
+          console.log(res.data)
+          this.recommends = res.data.slider
+        }
+      })
+    }
+  },
+  components: {
+    slider
+  }
+}
+
 </script>
+
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
