95de3a449300d582bebf1b9cb0073057b6015053
roy-lau <roy-lau@foxmail.com>
2017-10-14 16:55:27 +0800

    vue-music-mobile：获取歌手数据并进行拼装聚合成需要的数据

--- project/vue-music-mobile/src/components/recommend/recommend.vue
+++ project/vue-music-mobile/src/components/recommend/recommend.vue
@@ -59,7 +59,6 @@ export default {
     _getDiscList() {
       getDiscList().then((res) => {
         if (res.code === ERR_OK) {
-          console.log(res.data)
           this.discList = res.data.list
         }
       })
