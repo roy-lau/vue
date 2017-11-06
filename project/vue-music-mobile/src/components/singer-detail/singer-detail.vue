9dabb744cf7941dcf2caff44765ed4fed8f1942c
roy-lau <roy-lau@foxmail.com>
2017-11-06 14:17:59 +0800

    vue-music-mobile：歌单详情页 :add:

--- project/vue-music-mobile/src/components/singer-detail/singer-detail.vue
+++ project/vue-music-mobile/src/components/singer-detail/singer-detail.vue
@@ -1,8 +1,6 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail">
-        歌手详情页
-    </div>
+   <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
   </transition>
 </template>
 <script type="text/ecmascript-6">
@@ -10,9 +8,21 @@ import { mapGetters } from 'vuex'
 import { getSingerDetail } from 'api/singer'
 import { ERR_OK } from 'api/config'
 import { createSong } from 'common/js/song'
+import MusicList from 'components/music-list/music-list'
 
 export default {
+  data() {
+    return {
+      songs: []
+    }
+  },
   computed: {
+    title() {
+      return this.singer.name
+    },
+    bgImage() {
+      return this.singer.avatar
+    },
     ...mapGetters([
       'singer'
     ])
@@ -22,6 +32,7 @@ export default {
   },
   methods: {
     _getDetail() {
+      console.log(this.singer.name)
       if (!this.singer.id) {
         this.$router.push('/singer')
         return
@@ -29,7 +40,6 @@ export default {
       getSingerDetail(this.singer.id).then((res) => {
         if (res.code === ERR_OK) {
           this.songs = this._normalizeSongs(res.data.list)
-          console.log(this.songs)
         }
       })
     },
@@ -43,6 +53,9 @@ export default {
       })
       return ret
     }
+  },
+  components: {
+    MusicList
   }
 }
 
@@ -52,14 +65,6 @@ export default {
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
 
-  .singer-detail
-    position: fixed
-    z-index: 100
-    top: 0
-    left: 0
-    right: 0
-    bottom: 0
-    background: $color-background
   .slide-enter-active, .slide-leave-active
     transition: all 0.3s
   .slide-enter, .slide-leave-to
