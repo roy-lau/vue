8ad2a49e7294f7de52f4d870da0a0c50289e408b
roy-lau <roy-lau@foxmail.com>
2017-11-11 23:34:23 +0800

    vue-music-mobile：播放器显示/隐藏--动画 :musical_note:

--- project/vue-music-mobile/src/components/player/player.vue
+++ project/vue-music-mobile/src/components/player/player.vue
@@ -1,58 +1,66 @@
 <template>
   <div class="player" v-show="playList.length>0">
     <!-- 展开的播发器dom start-->
-    <div class="normal-player" v-show="fullScreen">
-      <!-- 背景图 -->
-      <div class="background"><img :src="currentSong.image" :alt="currentSong.name" width="100%" height="100%" /></div>
-      <!-- 头部 -->
-      <div class="top">
-        <div class="back" @click="back">
-          <i class="icon-back"></i>
+    <transition name="normal" @enter="enter" @after-enter="afterEnter" @leave="leave" @after-leave="afterLeave">
+      <div class="normal-player" v-show="fullScreen">
+        <!-- 背景图 -->
+        <div class="background"><img :src="currentSong.image" :alt="currentSong.name" width="100%" height="100%" /></div>
+        <!-- 头部 -->
+        <div class="top">
+          <div class="back" @click="back">
+            <i class="icon-back"></i>
+          </div>
+          <h1 class="title" v-html="currentSong.name"></h1>
+          <h2 class="subtitle" v-html="currentSong.singer"></h2>
         </div>
-        <h1 class="title" v-html="currentSong.name"></h1>
-        <h2 class="subtitle" v-html="currentSong.singer"></h2>
-      </div>
-      <!-- 中间部分（唱片） -->
-      <div class="middle">
-        <div class="middle-l">
-          <div class="cd-wrapper">
-            <div class="cd"><img :src="currentSong.image" :alt="currentSong.name" class="image" /></div>
+        <!-- 中间部分（唱片） -->
+        <div class="middle">
+          <div class="middle-l">
+            <div class="cd-wrapper" ref="cdWrapper">
+              <div class="cd"><img :src="currentSong.image" :alt="currentSong.name" class="image" /></div>
+            </div>
+          </div>
+        </div>
+        <!-- 底部操作区 -->
+        <div class="bottom">
+          <div class="operators">
+            <!-- 播放方式图标 -->
+            <div class="icon i-left"><i class="icon-sequence"></i></div>
+            <!-- 上一曲 -->
+            <div class="icon i-left"><i class="icon-prev"></i></div>
+            <!-- 播放&&暂停 -->
+            <div class="icon i-center"><i class="icon-play"></i></div>
+            <!-- 下一曲 -->
+            <div class="icon i-right"><i class="icon-next"></i></div>
+            <!-- 收藏&&心 -->
+            <div class="icon i-right"><i class="icon-not-favorite"></i></div>
           </div>
         </div>
       </div>
-      <!-- 底部操作区 -->
-      <div class="bottom">
-        <div class="operators">
-          <!-- 播放方式图标 -->
-          <div class="icon i-left"><i class="icon-sequence"></i></div>
-          <!-- 上一曲 -->
-          <div class="icon i-left"><i class="icon-prev"></i></div>
-          <!-- 播放&&暂停 -->
-          <div class="icon i-center"><i class="icon-play"></i></div>
-          <!-- 下一曲 -->
-          <div class="icon i-right"><i class="icon-next"></i></div>
-          <!-- 收藏&&心 -->
-          <div class="icon i-right"><i class="icon-not-favorite"></i></div>
-        </div>
-      </div>
-    </div>
+    </transition>
     <!-- 展开的播发器dom end -->
     <!-- 收起的播发器dom start -->
-    <div class="mini-player" v-show="!fullScreen" @click="open">
-      <div class="icon"><img :src="currentSong.image" :alt="currentSong.name" width="40" height="40" /></div>
-      <div class="text">
-        <h2 class="name" v-html="currentSong.name"></h2>
-        <p class="desc" v-html="currentSong.singer"></p>
+    <transition name="mini">
+      <div class="mini-player" v-show="!fullScreen" @click="open">
+        <div class="icon"><img :src="currentSong.image" :alt="currentSong.name" width="40" height="40" /></div>
+        <div class="text">
+          <h2 class="name" v-html="currentSong.name"></h2>
+          <p class="desc" v-html="currentSong.singer"></p>
+        </div>
+        <div class="control"></div>
+        <div class="control"><i class="icon-playlist"></i></div>
       </div>
-      <div class="control"></div>
-      <div class="control"><i class="icon-playlist"></i></div>
-    </div>
+    </transition>
     <!-- 收起的播发器dom end -->
   </div>
 </template>
 
 <script type="text/ecmascript-6">
 import { mapGetters, mapMutations } from 'vuex'
+import animations from 'create-keyframe-animation'
+import { prefixStyle } from 'common/js/dom'
+
+const transform = prefixStyle('transform')
 
 export default {
   computed: {
@@ -71,7 +79,64 @@ export default {
     },
     ...mapMutations({
       setFullScreen: 'SET_FULL_SCREEN'
-    })
+    }),
+    // 动画钩子 start
+    enter(el, done) {
+      const {x, y, scale} = this._getPosAndScale()
+
+      let animation = {
+        0: {
+          transform: `translate3d(${x}px,${y}px,0) scale(${scale})`
+        },
+        60: {
+          transform: `translate3d(0,0,0) scale(1.1)`
+        },
+        100: {
+          transform: `translate3d(0,0,0) scale(1)`
+        }
+      }
+
+      animations.registerAnimation({
+        name: 'move',
+        animation,
+        presets: {
+          duration: 400,
+          easing: 'linear'
+        }
+      })
+
+      animations.runAnimation(this.$refs.cdWrapper, 'move', done)
+    },
+    afterEnter() {
+      animations.unregisterAnimation('move')
+      this.$refs.cdWrapper.style.animation = ''
+    },
+    leave(el, done) {
+      this.$refs.cdWrapper.style.transition = 'all 0.4s'
+      const {x, y, scale} = this._getPosAndScale()
+      this.$refs.cdWrapper.style[transform] = `translate3d(${x}px,${y}px,0) scale(${scale})`
+      this.$refs.cdWrapper.addEventListener('transitionend', done)
+    },
+    afterLeave() {
+      this.$refs.cdWrapper.style.transition = ''
+      this.$refs.cdWrapper.style[transform] = ''
+    },
+    // 获取CD唱片的位置
+    _getPosAndScale() {
+      const targetWidth = 40
+      const paddingLeft = 40
+      const paddingBottom = 30
+      const paddingTop = 80
+      const width = window.innerWidth * 0.8
+      const scale = targetWidth / width
+      const x = -(window.innerWidth / 2 - paddingLeft)
+      const y = window.innerHeight - paddingTop - width / 2 - paddingBottom
+      return {
+        x,
+        y,
+        scale
+      }
+    }
   }
 }
 
