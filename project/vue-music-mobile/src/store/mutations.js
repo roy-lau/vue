d779c4d749341e7c94f1eac12a887ef2a4a57752
roy-lau <roy-lau@foxmail.com>
2017-11-11 14:27:50 +0800

    vue-music-mobile：播放器相关的vuex

--- project/vue-music-mobile/src/store/mutations.js
+++ project/vue-music-mobile/src/store/mutations.js
@@ -1,9 +1,27 @@
 import * as types from './mutation-types'
-
+// mutation 是同步函数
 const matutaions = {
   [types.SET_SINGER](state, singer) {
     state.singer = singer
-  }
+  },
+  [types.SET_PLAYING](state, flag) {
+    state.playing = flag
+  },
+  [types.SET_FULL_SCREEN](state, flag) {
+    state.fullScreen = flag
+  },
+  [types.SET_PLAYLIST](state, list) {
+    state.playList = list
+  },
+  [types.SET_SEQUENCE_LIST](state, list) {
+    state.sequenceList = list
+  },
+  [types.SET_PLAY_MODE](state, mode) {
+    state.mode = mode
+  },
+  [types.SET_CURRENT_INDEX](state, flag) {
+    state.currentIndex = flag
+  },
 }
 
 export default matutaions
--- project/vue-music-mobile/src/store/state.js
+++ project/vue-music-mobile/src/store/state.js
@@ -1,5 +1,13 @@
+import { playMode } from 'common/js/config'
+
 const state = {
-  stinger: {}
+  stinger: {},
+  playing: false, // 是否播放
+  fullScreen: false, // 是否全屏
+  playList: [], // 播放列表
+  sequenceList: [], // 顺序播放列表
+  mode: playMode.sequence, // 播放模式
+  currentIndex: -1
 }
 
 export default state
