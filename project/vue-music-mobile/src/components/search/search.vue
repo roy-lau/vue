c7534957f3677d3f074a6a8c852151ff15fef491
roy-lau <roy-lau@foxmail.com>
2017-12-23 23:57:45 +0800

    Title		: vue-music-mobile Subject		: 搜索列表组件 Description	: Tops		: :heavy_plus_sign:增 Author		: :man_with_turban: Roy-Lau

--- project/vue-music-mobile/src/components/search/search.vue
+++ project/vue-music-mobile/src/components/search/search.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="search">
     <div class="search-box-wrapper">
-      <search-box ref="searchBox"></search-box>
+      <search-box ref="searchBox" @query="onQueryChange"></search-box>
     </div>
-    <div class="shortcut-wrapper">
+    <div class="shortcut-wrapper" v-show="!query">
       <div class="shortcut">
         <div class="hot-key">
           <h1 class="title">热门搜索</h1>
@@ -13,6 +13,9 @@
         </div>
       </div>
     </div>
+    <div class="search-result" v-show="query">
+      <suggest :query="query"></suggest>
+    </div>
   </div>
 </template>
 
@@ -20,6 +23,7 @@
   import SearchBox from 'base/search-box/search-box'
   import { getHotKey } from 'api/search'
   import { ERR_OK } from 'api/config'
+  import Suggest from 'components/suggest/suggest'
 
   export default {
     created() {
@@ -27,12 +31,16 @@
     },
     data() {
       return {
-        hotKey: []
+        hotKey: [],
+        query: ''
       }
     },
     methods: {
       addQuery(query) {
         this.$refs.searchBox.setQuery(query)
+      },
+      onQueryChange(query) {
+        this.query = query
       },
       _getHotKey() {
         getHotKey().then(res => {
@@ -43,7 +51,8 @@
       }
     },
     components: {
-      SearchBox
+      SearchBox,
+      Suggest
     }
   }
 </script>
