7d8c4390be5c4bd3632c55f12a95491741112641
roy-lau <roy-lau@foxmail.com>
2019-02-25 18:01:02 +0800

    mall: express部署mall

---
+++ project/mall/server/dist/precache-manifest.8a51e41cfb456904f7102710c07d3738.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b09476835b9e08dda684081b12cd6f5",
+    "url": "/img/loading-bars.9b094768.svg"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a100f751c275fa39b74b",
+    "url": "/js/chunk-vendors.d50e9550.js"
+  },
+  {
+    "revision": "c9c3dc6b12211cd9a48c",
+    "url": "/js/cart.0436fc85.js"
+  },
+  {
+    "revision": "2e4b5eeb53b0061d193a",
+    "url": "/js/app.f3fcabed.js"
+  },
+  {
+    "revision": "5c19a02d90757561d99da6c209b5ef40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e739067b39ebec1dd28922ea2293b47b",
+    "url": "/img/mi6.e739067b.jpg"
+  },
+  {
+    "revision": "63d224460e6e676a93888a91bdb20e59",
+    "url": "/img/loading-spinning-bubbles.63d22446.svg"
+  },
+  {
+    "revision": "7ad0abc9e339045e6247f9359117359f",
+    "url": "/img/icon.7ad0abc9.png"
+  },
+  {
+    "revision": "c9c3dc6b12211cd9a48c",
+    "url": "/css/cart.188a24be.css"
+  },
+  {
+    "revision": "2e4b5eeb53b0061d193a",
+    "url": "/css/app.78082244.css"
+  }
+];
