f604cab977c9936e8d0f77b40c9281a84c093e97
roylau-mac <roylau-mac@163.com>
2019-03-10 18:51:49 +0800

    jdjr: 初期组件完成，header组件完成

---
+++ project/jdjr/src/components/slider.vue
@@ -0,0 +1,46 @@
+<template>
+  <section class="slide">
+    <swiper :options="options" not-next-tick="options.notNextTick">
+      <swiper-slide v-for="item in items" :key="item.href">
+          <router-link :to="item.href">
+              <img :    src="item.src" alt="">
+          </router-link>
+      </swiper-slide>
+      <div class="swiper-pagination" v-if="options.pagination"/>
+    </swiper>
+  </section>
+</template>
+
+<script>
+import { swiper, swiperSlide } from "vue-awesome-swiper";
+export default {
+  components: {
+    swiper,
+    swiperSlide
+  },
+  props: {
+    options: {
+      type: Object,
+      default: () => {
+          return{
+              pagination:{
+                  el:".swiper-pagination"
+              },
+              autoplay: true,
+              loop:true,
+              notNextTick: false,
+          }
+      }
+    },
+    items:{
+        type:Array,
+        default:()=>[]
+    }
+  }
+};
+</script>
+<style>
+@import "~swiper/dist/css/swiper.css";
+</style>
+
+
