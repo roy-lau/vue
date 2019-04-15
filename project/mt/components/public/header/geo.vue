80e75ca195ce48f1bce522ff41344f4790d413b4
roylau <roy-lau@foxmail.com>
2019-04-15 20:29:41 +0800

    mall: 新建mall分支

--- project/mt/components/public/header/geo.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="m-geo">
-        <i class="el-icon-location" /> 北京市
-        <nuxt-link class="changeCity" to="/changeCity"> 切换城市</nuxt-link>
-        [XX XX XX]
-    </div>
-</template>
