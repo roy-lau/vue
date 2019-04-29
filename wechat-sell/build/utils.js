353288ecf288bc7d8d4645457ddf50c1aed5cbe2
roy-lau <roy-lau@foxmail.com>
2019-04-29 14:49:23 +0800

    wechat-sell: :delete: px2remLoader

--- wechat-sell/build/utils.js
+++ wechat-sell/build/utils.js
@@ -21,13 +21,6 @@ exports.cssLoaders = function (options) {
       sourceMap: options.sourceMap
     }
   }
-  const px2remLoader = {
-    loader: 'px2rem-loader',
-    options: {
-      remUnit: 75,
-      remPrecision: 8
-    }
-  }
 
   const postcssLoader = {
     loader: 'postcss-loader',
@@ -38,7 +31,7 @@ exports.cssLoaders = function (options) {
 
   // generate loader string to be used with extract text plugin
   function generateLoaders (loader, loaderOptions) {
-    const loaders = options.usePostCSS ? [cssLoader, postcssLoader, px2remLoader] : [cssLoader,px2remLoader]
+    const loaders = options.usePostCSS ? [cssLoader, postcssLoader] : [cssLoader]
 
     if (loader) {
       loaders.push({
