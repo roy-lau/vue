c141eac0a3146e705e405f2ed668838b9b710c96
roy-lau <roylau_vip@163.com>
2020-07-04 13:50:03 +0800

    deploy: update config file

--- git-clone.sh
+++ git-clone.sh
@@ -6,3 +6,19 @@ git clone https://github.com/roy-lau/vue.git --depth 1 -b fund-back fund-back
 cd fund-back/server
 yarn
 cd ../..
+git clone git@github.com:roy-lau/vue.git --depth 1 -b fund-oracle fund-oracle
+cd fund-oracle
+yarn && yarn build
+cd ..
+git clone git@github.com:roy-lau/vue.git --depth 1 -b jdjr jdjr
+cd jdjr
+npm i && npm run build
+cd ..
+git clone git@github.com:roy-lau/vue.git --depth 1 -b mall mall
+cd mall
+yarn && yarn build 
+cd ..
+git clone git@github.com:roy-lau/vue.git --depth 1 -b mt-pc mt-pc
+cd mt-pc
+yarn && yarn build
+cd ..
--- pm2.config.js
+++ pm2.config.js
@@ -17,12 +17,56 @@ module.exports = {
         "script": "./server.js",
         "watch": true,
         "ignoreWatch": ["node_modules"],
-        "instances"  : "max",
-         "exec_mode": "cluster_mode",
         "log_date_format": "YYYY-MM-DD HH:mm:ss",
         "log_file": "../log/fund-back.log",
         "error_file": "../log/fund-back-err.log",
         "out_file": "../log/fund-back-out.log",
         "pid_file": "../log/fund-back.pid"
+    },{
+        "name": "fund-oracle",
+        "cwd": "./fund-oracle/dist",
+        "script": "hs -x ---'.' -p 3004",
+        "watch": true,
+        "ignoreWatch": ["node_modules"],
+	// "instances"  : "max", // 运行当前主机的最大核心
+        // "exec_mode": "cluster_mode", // 开启集群模式
+        "log_date_format": "YYYY-MM-DD HH:mm:ss",
+        "log_file": "../../log/fund-oracle.log",
+        "error_file": "../../log/fund-oracle-err.log",
+        "out_file": "../../log/fund-oracle-out.log",
+        "pid_file": "../../log/fund-oracle.pid"
+    },{
+        "name": "jdjr",
+        "cwd": "./jdjr/dist",
+        "script": "hs -x ---'.' -p 3005",
+        "watch": true,
+        "ignoreWatch": ["node_modules"],
+        "log_date_format": "YYYY-MM-DD HH:mm:ss",
+        "log_file": "../../log/jdjr.log",
+        "error_file": "../../log/jdjr-err.log",
+        "out_file": "../../log/jdjr-out.log",
+        "pid_file": "../../log/jdjr.pid"
+    },{
+        "name": "mall",
+        "cwd": "./mall/server",
+        "script": "app.js",
+        "watch": true,
+        "ignoreWatch": ["node_modules"],
+        "log_date_format": "YYYY-MM-DD HH:mm:ss",
+        "log_file": "../../log/mall.log",
+        "error_file": "../../log/mall-err.log",
+        "out_file": "../../log/mall-out.log",
+        "pid_file": "../../log/mall.pid"
+    },{
+        "name": "mt-pc",
+        "cwd": "./mt-pc",
+        "script": "yarn start",
+        // "watch": true,
+        "ignoreWatch": ["node_modules"],
+        "log_date_format": "YYYY-MM-DD HH:mm:ss",
+        "log_file": "./log/mt-pc.log",
+        "error_file": "./log/mt-pc-err.log",
+        "out_file": "./log/mt-pc-out.log",
+        "pid_file": "./log/mt-pc.pid"
     }]
 }
