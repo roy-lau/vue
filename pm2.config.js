b0bd9c021b1258820f6a751a61e8a6195efd3021
roy-lau <roylau_vip@163.com>
2022-04-14 20:40:00 +0800

    update: 更新pm2配置文件

--- pm2.config.js
+++ pm2.config.js
@@ -7,11 +7,11 @@ module.exports = {
         "watch": true,
         "ignoreWatch": ["node_modules"],
         "log_date_format": "YYYY-MM-DD HH:mm:ss",
-        "log_file": "../log/vue2.log",
-        "error_file": "../log/vue2-err.log",
-        "out_file": "../log/vue2-out.log",
-        "pid_file": "../log/vue2.pid"
-    },{
+        "log_file": "../log/music.log",
+        "error_file": "../log/music-err.log",
+        "out_file": "../log/music-out.log",
+        "pid_file": "../log/music.pid"
+    }, {
         "name": "fund-back",
         "cwd": "./fund-back/server",
         "script": "./server.js",
@@ -22,23 +22,23 @@ module.exports = {
         "error_file": "../log/fund-back-err.log",
         "out_file": "../log/fund-back-out.log",
         "pid_file": "../log/fund-back.pid"
-    },{
+    }, {
         "name": "fund-oracle",
         "cwd": "./fund-oracle/dist",
-        "script": "hs -x ---'.' -p 3004",
+        "script": "http-server -x ---'.' -p 3004",
         "watch": true,
         "ignoreWatch": ["node_modules"],
-	// "instances"  : "max", // 运行当前主机的最大核心
+        // "instances"  : "max", // 运行当前主机的最大核心
         // "exec_mode": "cluster_mode", // 开启集群模式
         "log_date_format": "YYYY-MM-DD HH:mm:ss",
         "log_file": "../../log/fund-oracle.log",
         "error_file": "../../log/fund-oracle-err.log",
         "out_file": "../../log/fund-oracle-out.log",
         "pid_file": "../../log/fund-oracle.pid"
-    },{
+    }, {
         "name": "jdjr",
         "cwd": "./jdjr/dist",
-        "script": "hs -x ---'.' -p 3005",
+        "script": "http-server -x ---'.' -p 3005",
         "watch": true,
         "ignoreWatch": ["node_modules"],
         "log_date_format": "YYYY-MM-DD HH:mm:ss",
@@ -46,7 +46,7 @@ module.exports = {
         "error_file": "../../log/jdjr-err.log",
         "out_file": "../../log/jdjr-out.log",
         "pid_file": "../../log/jdjr.pid"
-    },{
+    }, {
         "name": "mall",
         "cwd": "./mall/server",
         "script": "app.js",
@@ -57,15 +57,28 @@ module.exports = {
         "error_file": "../../log/mall-err.log",
         "out_file": "../../log/mall-out.log",
         "pid_file": "../../log/mall.pid"
-    },{
-        "name": "mt-pc",
-        "cwd": "./mt-pc",
-        "script": "npm run start",
+    }, 
+    // {
+    //     "name": "mt-pc",
+    //     "cwd": "./mt-pc",
+    //     "script": "npm run start",
+    //     "ignoreWatch": ["node_modules"],
+    //     "log_date_format": "YYYY-MM-DD HH:mm:ss",
+    //     "log_file": "../log/mt-pc.log",
+    //     "error_file": "../log/mt-pc-err.log",
+    //     "out_file": "../log/mt-pc-out.log",
+    //     "pid_file": "../log/mt-pc.pid"
+    // }, 
+    {
+        "name": "web-pages",
+        "cwd": "../web",
+        "script": "http-server -x ---'.' -p 3008",
+        "watch": true,
         "ignoreWatch": ["node_modules"],
         "log_date_format": "YYYY-MM-DD HH:mm:ss",
-        "log_file": "../log/mt-pc.log",
-        "error_file": "../log/mt-pc-err.log",
-        "out_file": "../log/mt-pc-out.log",
-        "pid_file": "../log/mt-pc.pid"
+        "log_file": "../web-logs/web-pages.log",
+        "error_file": "../web-logs/web-pages-err.log",
+        "out_file": "../web-logs/web-pages-out.log",
+        "pid_file": "../web-logs/web-pages.pid"
     }]
 }
